f226f2661705161dfc3985eb5af534bec31821de
wenbobuaa <wenbobuaa@foxmail.com>
2022-03-30 17:41:05 +0800

    refine init script

--- init_script.sh
+++ init_script.sh
@@ -1,8 +1,24 @@
 # zsh
-yum -y install zsh
+apt-get -y install zsh
 chsh -s /bin/zsh
 
+# git
+apt-get -y git
+
 # oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://gitee.com/mirrors/oh-my-zsh.git
+cd oh-my-zsh
+cd tools
+./install.sh
+
+# -- git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://gitee.com/lightnear/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# -- git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://gitee.com/xiaoqqya/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions 
+
+# conf
+git clone https://gitee.com/wenbobuaa/confbak.git
+
+cp confbak/.gitconfig ~/.
+cp confbak/.zshrc ~/.
+cp confbak/.vimrc ~/.
